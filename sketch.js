0c4ae0eacad00083054cab8e4ca15108f26a5608
Chestnut1122 <jili0710@uni.sydney.edu.au>
2025-06-06 15:50:39 +1000

    resize the canvas and add some comments

--- sketch.js
+++ sketch.js
@@ -1,8 +1,20 @@
+//draw the static Mondrian-style composition
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(windowWidth, windowHeight); //canvas size
   background(255);
-  noLoop();
+  noLoop();  
+  drawComposition();
+}
 
+//automatically called when the browser window is resized
+//resizes the canvas and redraws the composition
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  redraw();
+}
+
+//main drawing function
+function drawComposition(){
   let y = 0;
   let lastColor = null;
 
@@ -22,18 +34,19 @@ function setup() {
       }
 
       let colorList = [
+        [255, 255, 255],     //white
+        [255, 255, 255],     //higher probability for white
         [255, 255, 255],
         [255, 255, 255],
-        [255, 255, 255],
-        [255, 255, 255],
-        [255, 0, 0],
-        [255, 230, 90],
-        [0, 102, 255],
-        [0, 0, 0]
+        [255, 0, 0],         //red
+        [255, 230, 90],      //yellow
+        [0, 102, 255],       //blue
+        [0, 0, 0]            //black
       ];
 
       let chosenColor = null;
 
+      //select a color that is not same as the previous one
       while (true) {
         let index = Math.floor(random(colorList.length));
         let candidate = colorList[index];
@@ -63,6 +76,7 @@ function setup() {
   }
 }
 
+//check whether two colors are the same
 function colorsEqual(c1, c2) {
   return c1[0] === c2[0] && c1[1] === c2[1] && c1[2] === c2[2];
 }
